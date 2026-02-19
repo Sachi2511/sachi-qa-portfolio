9ef636f2d138deeefcb439461a4a0c79a76395a4
Sachi2511 <sachi.kayastha@gmail.com>
2026-02-19 17:45:29 +0000

    Add BaseTest class for TestNG setup and teardown

---
+++ automation/framework/src/test/java/Practice/BaseTest.java
@@ -0,0 +1,21 @@
+package Practice;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
+public class BaseTest {
+
+    @BeforeMethod
+    @Parameters({"browser"})
+    public void setUp(String browser) {
+        // Initialize browser using DriverFactory1
+        DriverFactory1.initDriver(browser);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        // Quit browser after each test
+        DriverFactory1.quitDriver();
+    }
+}
