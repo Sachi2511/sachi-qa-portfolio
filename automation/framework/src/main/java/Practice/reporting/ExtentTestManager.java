c9f37f787a1be8b8426ea44670d1af1d67135d6c
Sachi2511 <sachi.kayastha@gmail.com>
2026-03-04 13:29:01 +0000

    Update ExtentTestManager.java

--- automation/framework/src/main/java/Practice/reporting/ExtentTestManager.java
+++ automation/framework/src/main/java/Practice/reporting/ExtentTestManager.java
@@ -1,1 +1,17 @@
+package Practice.reporting;
 
+import com.aventstack.extentreports.ExtentTest;
+
+public class ExtentTestManager {
+
+    private static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
+
+    public static ExtentTest getTest() {
+        return test.get();
+    }
+
+    public static void startTest(String testName) {
+        ExtentTest extentTest = ExtentManager.getInstance().createTest(testName);
+        test.set(extentTest);
+    }
+}
