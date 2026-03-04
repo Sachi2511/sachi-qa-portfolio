c5d1552bc6babaf81a87ce7b6e2f11b0ea69a096
Sachi2511 <sachi.kayastha@gmail.com>
2026-03-04 13:33:25 +0000

    Update TestListener.java

--- automation/framework/src/test/java/Practice/listeners/TestListener.java
+++ automation/framework/src/test/java/Practice/listeners/TestListener.java
@@ -1,1 +1,30 @@
+package Practice.listeners;
 
+import Practice.reporting.ExtentManager;
+import Practice.reporting.ExtentTestManager;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class TestListener implements ITestListener {
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        ExtentTestManager.startTest(result.getMethod().getMethodName());
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        ExtentTestManager.getTest().pass("Test Passed");
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        ExtentTestManager.getTest().fail(result.getThrowable());
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        ExtentManager.getInstance().flush();
+    }
+}
