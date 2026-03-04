8d9074e908555d5c68a09b153473240a8dd81b77
Sachi2511 <sachi.kayastha@gmail.com>
2026-03-04 13:27:20 +0000

    Update ExtentManager.java

--- automation/framework/src/main/java/Practice/reporting/ExtentManager.java
+++ automation/framework/src/main/java/Practice/reporting/ExtentManager.java
@@ -1,1 +1,27 @@
 
+package Practice.reporting;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentManager {
+
+    private static ExtentReports extent;
+
+    public static ExtentReports getInstance() {
+        if (extent == null) {
+            createInstance("reports/ExtentReport.html");
+        }
+        return extent;
+    }
+
+    public static ExtentReports createInstance(String filePath) {
+        ExtentSparkReporter reporter = new ExtentSparkReporter(filePath);
+        reporter.config().setReportName("Automation Test Report");
+        reporter.config().setDocumentTitle("Test Execution Report");
+
+        extent = new ExtentReports();
+        extent.attachReporter(reporter);
+        return extent;
+    }
+}
