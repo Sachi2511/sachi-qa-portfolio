01e07c03a7596c2bf388f21c86e37efa192a7b19
Sachi2511 <sachi.kayastha@gmail.com>
2026-03-10 09:45:05 +0000

    Update DataProviders.java

--- automation/framework/src/main/java/Practice/utils/DataProviders.java
+++ automation/framework/src/main/java/Practice/utils/DataProviders.java
@@ -5,11 +5,12 @@ import org.testng.annotations.DataProvider;
 public class DataProviders {
 
     @DataProvider(name = "loginData")
-    public Object[][] getLoginData() {
-        return new Object[][]{
-                {"standard_user", "secret_sauce"},
-                {"locked_out_user", "secret_sauce"},
-                {"problem_user", "secret_sauce"}
-        };
-    }
+public Object[][] getLoginData() {
+    return new Object[][]{
+            {"standard_user", "secret_sauce", "Products"},
+            {"locked_out_user", "secret_sauce", "Epic sadface: Sorry, this user has been locked out."},
+            {"problem_user", "secret_sauce", "Products"}
+    };
 }
+}
+
