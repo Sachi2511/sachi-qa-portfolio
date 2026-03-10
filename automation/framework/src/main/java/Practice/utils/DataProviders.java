e6b0134177f37d45272b9f0b3efa33d3186ade87
Sachi2511 <sachi.kayastha@gmail.com>
2026-03-10 09:32:48 +0000

    Create DataProviders.java

---
+++ automation/framework/src/main/java/Practice/utils/DataProviders.java
@@ -0,0 +1,15 @@
+package Practice.utils;
+
+import org.testng.annotations.DataProvider;
+
+public class DataProviders {
+
+    @DataProvider(name = "loginData")
+    public Object[][] getLoginData() {
+        return new Object[][]{
+                {"standard_user", "secret_sauce"},
+                {"locked_out_user", "secret_sauce"},
+                {"problem_user", "secret_sauce"}
+        };
+    }
+}
